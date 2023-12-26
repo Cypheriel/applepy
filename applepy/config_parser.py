1487b1eb3fc574e967c59c8f3fcf21e3787bbc6c
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2023-12-26 01:36:01 -0700

    feat(config): allow importing of signing/encryption keys

--- applepy/config_parser.py
+++ applepy/config_parser.py
@@ -9,7 +9,14 @@ from dotenv import set_key
 from applepy.albert import PUSH_CERTIFICATE_PATH, PUSH_KEY_PATH
 from applepy.apns import APNS_DOTENV
 from applepy.crypto_helper import save_certificate, save_private_key
-from applepy.ids import AUTH_CERT_PATH, AUTH_KEY_PATH, IDS_DOTENV, REGISTRATION_CERT_PATH
+from applepy.ids import (
+    AUTH_CERT_PATH,
+    AUTH_KEY_PATH,
+    ENCRYPTION_KEY_PATH,
+    IDS_DOTENV,
+    REGISTRATION_CERT_PATH,
+    SIGNING_KEY_PATH,
+)
 
 
 def import_credentials(path: Traversable) -> None:
@@ -27,6 +34,9 @@ def import_credentials(path: Traversable) -> None:
     save_certificate(AUTH_CERT_PATH, load_pem_x509_certificate(config["auth"]["cert"].encode()))
 
     save_certificate(REGISTRATION_CERT_PATH, load_pem_x509_certificate(config["id"]["cert"].encode()))
+
+    save_private_key(SIGNING_KEY_PATH, load_pem_private_key(config["encryption"]["ec_key"].encode(), password=None))
+    save_private_key(ENCRYPTION_KEY_PATH, load_pem_private_key(config["encryption"]["rsa_key"].encode(), password=None))
 
     set_key(APNS_DOTENV, "PUSH_TOKEN", config["push"]["token"])
     set_key(IDS_DOTENV, "PROFILE_ID", config["auth"]["user_id"])
